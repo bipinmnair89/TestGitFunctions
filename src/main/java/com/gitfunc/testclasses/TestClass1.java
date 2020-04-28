146fad9619ee0b528f84bd208d9eccfb5fe44b8a
bipinmnair89 <bipinmnair89@gmail.com>
2020-04-28 12:17:18 +0530

    The first project commit with the class - TestClass1.java

---
+++ src/main/java/com/gitfunc/testclasses/TestClass1.java
@@ -0,0 +1,11 @@
+package com.gitfunc.testclasses;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("First line comment in class TestClass1");
+	}
+
+}
