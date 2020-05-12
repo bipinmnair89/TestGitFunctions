324fba33b711fc7024c2b96ff717b8b491a0ac1a
bipinmnair89 <bipinmnair89@gmail.com>
2020-05-12 18:56:53 +0530

    This commit can be ignored as the changes were inappropriate

--- src/main/java/com/gitfunc/testclasses/TestClass2.java
+++ src/main/java/com/gitfunc/testclasses/TestClass2.java
@@ -5,7 +5,6 @@ public class TestClass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("comment 1");
-		System.out.println("comment A");
 	}
 
 }
