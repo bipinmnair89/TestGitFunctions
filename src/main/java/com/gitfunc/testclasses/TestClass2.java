4f64f23b7746bec90184df4640d3b0125147f429
bipinmnair89 <bipinmnair89@gmail.com>
2020-05-13 11:17:12 +0530

    comment 2 added in the class TestClass2.java

--- src/main/java/com/gitfunc/testclasses/TestClass2.java
+++ src/main/java/com/gitfunc/testclasses/TestClass2.java
@@ -5,6 +5,7 @@ public class TestClass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("comment 1");
+		System.out.println("comment 2");
 	}
 
 }
